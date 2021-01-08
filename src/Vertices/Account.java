9625a2d888be63e0e83f4e6fdcacf1e6cae6b36c
mohammad7hosein <mohamad77hosein77@gmail.com>
2021-01-08 20:04:50 +0330

    create print method Call...

--- src/Vertices/Account.java
+++ src/Vertices/Account.java
@@ -39,7 +39,7 @@ public class Account extends DirectedGraph.Vertex {
         String[] tableColumn = {"ردیف", "کد ملی صاحب حساب", "نام و نام خانوادگی صاحب حساب" , "نام بانک", "شماره شبا", "شماره حساب"};
         String[][] data = new String[allAccounts.size()][tableColumn.length];
         int i =0;
-        for (Account account : new ArrayList<>(allAccounts.values()) ) {
+        for (Account account : allAccounts.values() ) { // test here...
             People owner = (People) Main.directedGraph.getVertexByID(account.getOwnerSsn());
             data[i][0] = (i+1) + "";
             data[i][1] = account.getOwnerSsn();
