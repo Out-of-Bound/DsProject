589513412c803888009e81e4a889d79f68442361
mohammad7hosein <mohamad77hosein77@gmail.com>
2021-01-09 13:29:01 +0330

    create print method Call...

---
+++ src/App/Menu.java
@@ -0,0 +1,52 @@
+package App;
+
+import javax.swing.*;
+
+public class Menu {
+
+    private JPanel panel;
+    private JButton phase1;
+    private JButton phase2;
+    private JButton phase3;
+    private JButton phase4;
+    private JButton logout;
+
+    public Menu() {
+        JFrame MenuFrame = new JFrame("Menu");
+        MenuFrame.setContentPane(panel);
+        MenuFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        MenuFrame.setSize(600,500);
+        MenuFrame.setVisible(true);
+        MenuFrame.setLocationRelativeTo(null);
+
+        phase1.addActionListener(e -> {
+            new PhaseOne(MenuFrame);
+            MenuFrame.setEnabled(false);
+        });
+        phase2.addActionListener(e -> {
+
+            MenuFrame.setEnabled(false);
+        });
+        phase3.addActionListener(e -> {
+
+            MenuFrame.setEnabled(false);
+        });
+        phase4.addActionListener(e -> {
+
+            MenuFrame.setEnabled(false);
+        });
+        logout.addActionListener(e -> {
+            if (JOptionPane.showConfirmDialog(MenuFrame, "Are you sure you want to exit?" , "Exit", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION)
+                System.exit(0);
+        });
+
+        MenuFrame.addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                if (JOptionPane.showConfirmDialog(MenuFrame, "Are you sure you want to exit?" , "Exit", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION)
+                    System.exit(0);
+            }
+        });
+    }
+
+}
---
+++ src/App/PhaseOne.java
@@ -0,0 +1,66 @@
+package App;
+
+import Vertices.*;
+import javax.swing.*;
+
+public class PhaseOne {
+
+    private JPanel panel;
+    private JButton peopleButton;
+    private JButton phonesButton;
+    private JButton homesButton;
+    private JButton carsButton;
+    private JButton accountsButton;
+    private JButton callsButton;
+    private JButton relationshipsButton;
+    private JButton ownershipsButton;
+    private JButton transactionsButton;
+
+    public PhaseOne(JFrame MenuFrame){
+        JFrame PhaseOneFrame = new JFrame("PhaseOne");
+        PhaseOneFrame.setContentPane(panel);
+        PhaseOneFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        PhaseOneFrame.setSize(600,700);
+        PhaseOneFrame.setVisible(true);
+        PhaseOneFrame.setLocationRelativeTo(null);
+
+        peopleButton.addActionListener(e -> {
+            People.print();
+        });
+        phonesButton.addActionListener(e -> {
+
+        });
+        homesButton.addActionListener(e -> {
+            Home.print();
+        });
+        carsButton.addActionListener(e -> {
+            Car.print();
+        });
+        accountsButton.addActionListener(e -> {
+            Account.print();
+        });
+        callsButton.addActionListener(e -> {
+
+        });
+        relationshipsButton.addActionListener(e -> {
+
+        });
+        ownershipsButton.addActionListener(e -> {
+
+        });
+        transactionsButton.addActionListener(e -> {
+
+        });
+
+        PhaseOneFrame.addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                if (JOptionPane.showConfirmDialog(PhaseOneFrame, "Are you sure you want to exit?" , "Exit", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
+                    PhaseOneFrame.setVisible(false);
+                    MenuFrame.setEnabled(true);
+                }
+            }
+        });
+
+    }
+}
