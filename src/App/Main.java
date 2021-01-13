19a396167d5f6b6d04e7adf75b4a6f58c3d22914
amirb <amirbagheri322@yahoo.com>
2021-01-13 19:05:50 +0330

    little changes

--- src/App/Main.java
+++ src/App/Main.java
@@ -9,11 +9,11 @@ public class Main {
     public static DirectedGraph directedGraph = new DirectedGraph();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        try {
+        /*try {
             ReadFiles.start();
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
         new Menu();
     }
 }
